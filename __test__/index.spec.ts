2c1ded5af8a88a2723a1632e305d60967255097f
Joris <joris.gallot18@gmail.com>
2025-11-30 23:32:09 +0100

    test: add test for simple export

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,7 +1,14 @@
 import test from 'ava'
-
+import path from 'node:path'
 import { sweepy } from '../index'
 
-test('is defined', (t) => {
-  t.truthy(sweepy)
+test('simple export', (t) => {
+  const root = path.resolve(import.meta.dirname, 'fixtures', 'export-simple')
+  const entry = path.join(root, 'index.ts')
+  const res = sweepy(root, [entry])
+
+  t.deepEqual(res, {
+    reachableFiles: ['index.ts', 'export-simple.ts'],
+    unusedExports: []
+  })
 })
